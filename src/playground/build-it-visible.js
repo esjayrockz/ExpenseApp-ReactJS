f3312cdd4186ce04f91f33ac7172b714b4336507
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-03-12 18:02:33 -0400

    Integrated components

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,23 +1,31 @@
-const appRoot = document.getElementById('app');
-let visibility = false;
-const showDetails = () => {
-   visibility = !visibility;
-   renderReact();
-}
+class VisibilityToggle extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
 
-const renderReact = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick ={showDetails}>{visibility ? 'Hide Details' : 'Show Details'}</button>
+  handleToggleVisibility(){
+    this.setState((prevState)=>{
+      return {
+        visibility: !prevState.visibility
+      };
+    });
+  }
 
+  render(){
+    return (
+      <div>
+        <h1>{this.props.header}</h1>
+        <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
         {
-          visibility && (<div><p>Here are the details.</p></div>)
+          this.state.visibility && (<div><p>Here are the details</p></div>)
         }
-
-    </div>
-  );
-  ReactDOM.render(template,appRoot);
+      </div>
+    )
+  }
 }
 
-renderReact();
+ReactDOM.render(<VisibilityToggle header='Visibility Toggle' />, document.getElementById('app'));
