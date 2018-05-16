b49855fba65bebb00c71b4cd1069603b7184fabe
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-15 20:51:34 -0400

    Setting up actions and reducers

---
+++ src/reducers/expenses.js
@@ -0,0 +1,28 @@
+const expensesReducerDefaultState = []; // Create variable to store default values
+
+export default (state = expensesReducerDefaultState, action)=>{
+  switch(action.type){
+    case 'ADD_EXPENSE':
+    return [...state, action.expense]; //using spread operator or concat instead of push to avoid changing original state
+    //returning an array with updated expenses as the expenses state is an array and not an object
+
+    case 'REMOVE_EXPENSE':
+    return state.filter(({id}) => id !== action.id);
+
+    case 'EDIT_EXPENSE':
+    return state.map((expense)=>{
+      if(expense.id === action.id){
+        return {
+          ...expense,
+          ...action.updates
+        };
+      }
+        else
+        return expense;
+      }
+    )
+
+    default:
+    return state;
+  }
+};
