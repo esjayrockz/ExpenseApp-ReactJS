f6f9563160d7067722cc67dc7043f19fb5006b85
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-16 22:32:18 -0400

    Learning higher order components

---
+++ src/playground/hoc.js
@@ -0,0 +1,10 @@
+//Higher Order Components
+import react from 'react';
+import reactDOM from 'react-dom';
+
+const info = (props) => (
+<div>
+  <h1>Info</h1>
+  <p>The info is {props.info}</p>
+</div>  
+);
