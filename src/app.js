03a16a1885a507c90fe30f63e113941151431692
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-16 22:07:14 -0400

    Checking functionality of redux after organizing redux sections

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-console.log(store.getState());
+store.subscribe(()=>{
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+
+});
+
+store.dispatch(addExpense({description:'Water bill', amount: 5000, createdAt: 5000}));
+store.dispatch(addExpense({description:'Gas bill', amount: 3000, createdAt: 10000}));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
