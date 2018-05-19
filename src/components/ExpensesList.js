d2f1626abba02e325b8dc7ef6d8e2b32ea7101c2
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-18 23:54:44 -0400

    Accessing complete Redux store and state in React

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -4,13 +4,15 @@ import {connect} from 'react-redux';
 const ExpensesList = (props) => (
   <div>
     <h1>List of Expenses</h1>
+    <p>{props.filters.text}</p>
     {props.expenses.length}
   </div>
 );
 
 const ConnectedExpensesList = connect((state)=>{
   return {
-    expenses: state.expenses
+    expenses: state.expenses,
+    filters: state.filters
   };
 })(ExpensesList);
 
