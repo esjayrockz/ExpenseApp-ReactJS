0652d9371264d14ab29e0bff02916c2b16070180
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-21 00:06:45 -0400

    Setting up form for adding/editing expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -11,6 +11,17 @@ export default class ExpenseForm extends React.Component {
             placeholder="Description"
             autoFocus
           />
+          <input
+            type="number"
+            placeholder="Amount"
+          />
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+            >
+            </textarea>
+          <button>
+            Add Expense
+          </button>
         </form>
       </div>
     )
