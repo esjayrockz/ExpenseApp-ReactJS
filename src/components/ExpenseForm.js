301afbf306eeb52dfc719ac62a15b4bea0bf551d
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-21 01:05:38 -0400

    Setting up onChange methods for Description and Note fields

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 export default class ExpenseForm extends React.Component {
 
+  state = {
+    description : '',
+    note: ''
+  };
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(()=>({ description }));
+  }
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(()=>({ note }));
+  }
+
   render(){
     return (
       <div>
@@ -10,6 +25,8 @@ export default class ExpenseForm extends React.Component {
             type="text"
             placeholder="Description"
             autoFocus
+            value = {this.state.description}
+            onChange = {this.onDescriptionChange}
           />
           <input
             type="number"
@@ -17,6 +34,8 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            value = {this.state.note}
+            onChange = {this.onNoteChange}
             >
             </textarea>
           <button>
