8ca86dc978c6db56071c99bf497de8b07f0d6c15
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-05-22 19:47:47 -0400

    Adding redux dev tool code in CreateStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,8 @@ export default () => {
   const store = createStore(combineReducers({ //Combine multiple reducers
     expenses: expensesReducer,
     filters: filtersReducer
-  })
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
   return store;
 };
