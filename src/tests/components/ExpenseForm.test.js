294b0c49d202a3f96e03de7f347a55dbcd5eb469
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-05 17:20:47 -0400

    Testing form input areas with valid and invalid input and event changes using Jest & Enzyme

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -24,10 +24,37 @@ test('should render error for invalid form submission', () => {
 });
 
 test('should set description on input change', () => {
-  const value = 'New Description';
+  const value = 'No Description';
   const wrapper = shallow(<ExpenseForm />);
   wrapper.find('input').at(0).simulate('change', {
     target: { value }
   });
   expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set note on textarea change', () => {
+  const value = 'note';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount if valid input', () => {
+  const value = '23.56';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should not set amount if invalid input', () => {
+  const value = '12.122';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('amount')).not.toBe(value);
 });
