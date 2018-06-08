88db2f7649de7733e7d4d865d70c1c9810567535
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-07 20:32:02 -0400

    Adding enzyme test cases for ExpenseListFilters

--- src/tests/components/ExpensesListFilters.test.js
+++ src/tests/components/ExpensesListFilters.test.js
@@ -28,6 +28,47 @@ test('should render ExpensesListFilters correctly', () => {
 });
 
 test('should render ExpensesListFilters with alternate expenses correctly', () => {
-  wrapper.setProps({ filters:  altFilters });
+  wrapper.setProps({ filters: altFilters });
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle text change', () => {
+  const value = altFilters.text;
+  wrapper.find('input').simulate('change', {
+    target: { value }
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+  wrapper.setProps({ filters: altFilters });
+  const value = filters.sortBy;
+  wrapper.find('select').simulate('change', {
+    target: {
+      value
+    }
+  });
+  expect(sortByDate).toHaveBeenCalled();
+});
+
+test('should sort by amount', () => {
+  const value = altFilters.sortBy;
+  wrapper.find('select').simulate('change', {
+    target: {
+      value
+    }
+  });
+  expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+  wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')({ startDate: altFilters.startDate, endDate: altFilters.endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(altFilters.startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(altFilters.endDate);
+});
+
+test('should handle date focus changes', () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('withStyles(DateRangePicker)').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
