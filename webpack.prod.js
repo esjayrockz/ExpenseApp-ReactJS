1187687e0251631151468a55cd8179a0eabd073a
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-10 03:38:38 -0400

    Testing various configs in prod to see why app is not running live

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,6 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
-  mode: 'production',
   plugins: [
       new UglifyJSPlugin({
         sourceMap: true
