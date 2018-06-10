823b5459751b2ce9c1857442d93656b31bc44beb
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-10 04:20:24 -0400

    Removing explicit mention of UglifyJSplugin from webpack prod file causing our app to not render properly

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
