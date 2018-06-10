0470ba37a895d30bfea92d1fb7f0ed7b3952f766
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-10 15:08:30 -0400

    Adding favicon icon

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
   plugins: [
+      new UglifyJSPlugin(),
       new webpack.DefinePlugin({
        'process.env.NODE_ENV': JSON.stringify('production')
      })
