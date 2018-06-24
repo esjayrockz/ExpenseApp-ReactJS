37c3804a5c2c08f7fbb1f6e84745c4d18601e769
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-23 23:26:29 -0400

    Setting up test suite for Thunk and firebase tasks for adding expenses

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -7,6 +7,15 @@ const promise = new Promise((resolve, reject) => {
 
 console.log('before');
 promise.then((data)=>{
-  console.log(data);
+  console.log('1 ', data);
+    new Promise((resolve, reject) => {
+    setTimeout(() => {
+    resolve('Yes');
+  },2500);
+  })
+}).then((str)=>{
+  console.log('Does this run?', str);
+}).catch((error)=>{
+  console.log('Ooo there was an error!', error);
 });
 console.log('after');
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,8 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = merge(common, {
  devtool: 'inline-source-map',
  devServer: {
