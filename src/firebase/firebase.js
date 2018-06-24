486fb3675607e775bcfb0f8522734fe786f20815
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-06-24 02:38:58 -0400

    Creating separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBNWInsEY92LKMk4tqZ1zJbLI1wj45qfNE",
-  authDomain: "yourexpenses-38a73.firebaseapp.com",
-  databaseURL: "https://yourexpenses-38a73.firebaseio.com",
-  projectId: "yourexpenses-38a73",
-  storageBucket: "yourexpenses-38a73.appspot.com",
-  messagingSenderId: "420347492640"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if(process.env.NODE_ENV === 'test'){
+  require('dotenv').config({ path: '.env.test' });
+}
+else if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = merge(common, {
  devtool: 'inline-source-map',
@@ -11,5 +19,15 @@ module.exports = merge(common, {
    historyApiFallback: true,
    publicPath: '/dist/'
  },
- mode: 'development'
+ mode: 'development',
+ plugins: [
+   new webpack.DefinePlugin({
+   'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+   'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+   'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+   'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+   'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+   'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+ })
+]
 });
