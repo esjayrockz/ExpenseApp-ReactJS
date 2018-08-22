f273aab498aa49cd0100bac736a66424ac8cb4e2
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-08-22 19:32:41 -0400

    Adding LoadingPages, test cases and babel-polyfill to support IE browser

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -5,26 +5,28 @@ import visibleExpenses from '../selectors/expenses';
 
 export const ExpensesList = (props) => (
   <div className="content-container">
-    <div className="list-header">
-      <div className="show-for-mobile">Expenses</div>
-      <div className="show-for-desktop">Expense</div>
-      <div className="show-for-desktop">Amount</div>
-    </div>
-
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ):
-      (
-        props.expenses.map((expense, index)=>(//map will take array of objects and return array of instances of expenseList item
-          <ExpenseItem
-            key = {expense.id}
-            {...expense}
-          />
-        ))
-      )
-    }
-
+      <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+      </div>
+      <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No expenses</span>
+          </div>
+        ):
+        (
+          props.expenses.map((expense, index)=>(//map will take array of objects and return array of instances of expenseList item
+            <ExpenseItem
+              key = {expense.id}
+              {...expense}
+            />
+          ))
+        )
+      }
+     </div>
   </div>
 );
 
